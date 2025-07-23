329f94cf1e2b941dc2078d97f6a9d9371ff9ddf5
Rafael <rfm777@protonmail.com>
2025-07-23 13:18:45 -0300

    Java O.O

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -26,18 +26,18 @@ public class Pedido {
         System.out.println("------- RESUMO PEDIDO -------");
         for (ItemPedido item : itens) {
             Produto p = item.getProduto();
-            double preco = Math.round(p.obterPrecoLiquido() * 100.0) / 100.0;
+            double preco = p.obterPrecoLiquido();
             int qtd = item.getQuantidade();
-            double totalItem = Math.round(preco * qtd * 100.0) / 100.0;
+            double totalItem = preco * qtd;
             totalProdutos += totalItem;
 
             String tipo = (p instanceof Livro) ? "Livro" : (p instanceof Dvd ? "Dvd" : "Produto");
             System.out.printf("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",
                     tipo, p.getTitulo(), preco, qtd, totalItem);
         }
-        totalProdutos = Math.round(totalProdutos * 100.0) / 100.0;
-        double desconto = Math.round(totalProdutos * percentualDesconto / 100.0 * 100.0) / 100.0;
-        double totalPedido = Math.round((totalProdutos - desconto) * 100.0) / 100.0;
+        totalProdutos = totalProdutos;
+        double desconto = totalProdutos * percentualDesconto/ 100.0;
+        double totalPedido = (totalProdutos - desconto) / 100.0;
         System.out.println("----------------------------");
         System.out.printf("DESCONTO: %.2f\n", desconto);
         System.out.printf("TOTAL PRODUTOS: %.2f\n", totalProdutos);
