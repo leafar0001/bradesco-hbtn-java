9156f352a960da6f1329c8db1b61906896c22d2a
Rafael <rfm777@protonmail.com>
2025-07-23 19:33:51 -0300

    Java O.O

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -22,15 +22,18 @@ public class PessoasArray {
             throw new IllegalArgumentException("O nome fornecido é nulo.");
         } 
 System.out.println("Procurando pelo nome: \"" + nome + "\"");
+        boolean encontrado = false;
 
         for (int i = 0; i < nomes.length; i++) {
 		System.out.println("Passando pelo indice:" + i);
             if (nomes[i] != null && nomes[i].equalsIgnoreCase(nome.trim())) {
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
-            }
+                encontrado = true;            }
+	
         }
-
-        throw new IllegalArgumentException("Nome " + nome + " não encontrado na lista.");
+if (!encontrado) {
+            throw new IllegalArgumentException("Nome \"" + nome + "\" não encontrado na lista.");
+        }
     }
 }
 //if \(nomes\[i\] == \w+\)
