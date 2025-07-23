b38e67adce3a11d782e0c1895b72fda4a1bd83a9
Rafael <rfm777@protonmail.com>
2025-07-23 16:20:33 -0300

    Java O.O

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,17 @@
+public class Numero {
+
+public static int dividir(int a, int b){
+
+int resultado = 0;
+        try {
+            resultado = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Nao eh possivel dividir por zero");
+        } finally {
+            System.out.printf("%d / %d = %d\n", a, b, resultado);
+        }
+return resultado;
+
+    }
+}
+
