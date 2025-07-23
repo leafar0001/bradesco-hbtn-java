f905928ebf91d597a0c4946eaf2f2586f12b6f5b
Rafael <rfm777@protonmail.com>
2025-07-23 11:23:59 -0300

    Java O.O

--- Heranca/forma/Program.java
+++ Heranca/forma/Program.java
@@ -1,9 +1,19 @@
 public class Program {
     public static void main(String[] args) {
-        FormaGeometrica formaGeometrica = new FormaGeometrica();
+        Retangulo retangulo = new Retangulo();
+        retangulo.setLargura(10);
+        retangulo.setAltura(2);
+
+        if (FormaGeometrica.class.isAssignableFrom(Retangulo.class))
+            System.out.println("Retangulo eh uma subclasse de FormaGeometrica");
+        else
+            System.out.println("Retangulo nao eh uma subclasse de FormaGeometrica");
+
+        System.out.printf("Largura: %.2f\n", retangulo.getLargura());
+        System.out.printf("Altura: %.2f\n", retangulo.getAltura());
 
         try {
-            System.out.printf("Area: %d", formaGeometrica.area());
+            System.out.printf("Area: %f\n", retangulo.area());
         } catch (Exception e) {
             System.out.println(e);
         }
