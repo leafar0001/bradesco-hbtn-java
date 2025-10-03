823bd3d5bf49b6122981c229a32a1b148761de45
Rafael <rfm777@protonmail.com>
2025-10-03 20:27:23 -0300

    Exercício 2

--- 0x19/acima_40/ConsultaPessoas.java
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -17,12 +17,24 @@ class ConsultaPessoas {
                       ));
     }
 
-    // Agrupa pessoas pelo cargo filtrando apenas quem tem idade acima de 40 anos
-    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
-        return pessoas.stream()
-                      .collect(Collectors.groupingBy(
-                          Pessoa::getCargo,
-                          Collectors.filtering(p -> p.getIdade() > 40, Collectors.toList())
-                      ));
-    }
+
+
+public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+    // Obtém todos os cargos únicos da lista
+    Set<String> cargos = pessoas.stream()
+                                .map(Pessoa::getCargo)
+                                .collect(Collectors.toSet());
+
+    // Cria o Map garantindo todos os cargos
+    Map<String, List<Pessoa>> map = cargos.stream()
+        .collect(Collectors.toMap(
+            cargo -> cargo,
+            cargo -> pessoas.stream()
+                            .filter(p -> p.getCargo().equals(cargo) && p.getIdade() > 40)
+                            .collect(Collectors.toList())
+        ));
+
+    return map;
+}
+
 }
