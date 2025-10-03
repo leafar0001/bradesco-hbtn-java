8c9307067b3b84c0ae04465d6fd620797a3b95e4
Rafael <rfm777@protonmail.com>
2025-10-03 17:58:34 -0300

    Exercício 2

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -30,6 +30,8 @@ public class PessoasArray {
             }
         }
 
-       
+        if (!encontrado) {
+            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
     }
 }
