834290cd3a92afca416da6ff54751484689eaa12
Rafael <rfm777@protonmail.com>
2025-10-03 20:36:58 -0300

    Exercício 2

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+// Classe Pessoa
+class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        // Ordena por código (ou qualquer critério desejado)
+        return Integer.compare(this.codigo, outra.codigo);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario);
+    }
+}
